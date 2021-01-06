24e85e6fa1af0c9a4d6178aeaad2c091f74afe50
rodrigo pichardo <hectorrodrigo2017@gmail.com>
2021-01-05 18:15:36 -0600

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 // Asi es como se hace una importacion en node js
 const express = require('express');
@@ -29,6 +30,12 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+
+// Lo ùltimo 
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 
 
